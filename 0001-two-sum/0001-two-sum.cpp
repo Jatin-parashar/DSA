17c8412ae87c901bccf36122d5212210a58ce01a
Jatin Parashar <83947754+Jatin-parashar@users.noreply.github.com>
2022-10-12 13:27:22 +0530

    Time: 25 ms (63.42%), Space: 11 MB (32.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        unordered_map<int,int> mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int val=target-arr[i];
+            if(mp.count(val)){
+                return {mp[val],i};
+            }
+            mp[arr[i]]=i;
+        }
+        return {};
+    }
+};
