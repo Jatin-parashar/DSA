175116de1d2311cf0fb46100d96d27931aef8743
Jatin Parashar <83947754+Jatin-parashar@users.noreply.github.com>
2022-10-19 12:50:19 +0530

    Time: 20 ms (68.83%), Space: 13.2 MB (5.47%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> ans;
+        unordered_map<string,int> mp;
+        for(int i=0;i<words.size();i++){
+            mp[words[i]]++;
+        }
+        for(auto x:mp){
+            cout<<x.first<<" "<<x.second<<endl;
+        }
+        map<int,multiset<string>,greater<int>> mp2;
+        for(auto x:mp){
+            mp2[x.second].insert(x.first);
+        }
+        for(auto x:mp2){
+            while(!x.second.empty()){
+                if(k==0) break;
+                ans.push_back(*x.second.begin());
+                x.second.erase(x.second.begin());
+                k--;
+            }
+
+        }
+        return ans;
+    }
+};
